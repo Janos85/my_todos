dd4a0d99d697e3a2c1152a511b85d460f884492a
Janos85 <janosbalogh85@gmail.com>
2024-07-29 15:04:44 +0200

    fire base tix

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:my_todos/firebase_options.dart';
 import 'package:my_todos/src/app.dart';
-import 'package:my_todos/src/firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
